4bb4484bee1c5fb15f00ff082fd3ef997716249a
waikhit <limwaikhit@gmail.com>
2018-09-14 16:29:09 +0800

    uploading solution

---
+++ solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import collections
+import sys
+
+def main():
+    letter_positions = collections.defaultdict(list)
+    print(letter_positions)
+
+
+if __name__ == '__main__':
+    main()
+
+
